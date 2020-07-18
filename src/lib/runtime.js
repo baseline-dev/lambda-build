ceb8f5962f4bb679501e0a195044f7bb683bb148
Nikolai Onken <nikolai@nikolaionken.com>
2020-07-18 11:50:53 +0200

    Add all-handler

--- src/lib/runtime.js
+++ src/lib/runtime.js
@@ -13,25 +13,20 @@ app.use(async (ctx, next) => {
   switch (ctx.request.method) {
     case 'GET':
       if (typeof module.get !== 'undefined') handler = module.get();
-    break;
     case 'POST':
       if (typeof module.post !== 'undefined') handler = module.post();
-    break;
     case 'PUT':
       if (typeof module.put !== 'undefined') handler = module.put();
-    break;
     case 'DELETE':
       if (typeof module.destroy !== 'undefined') handler = module.destroy();
-    break;
     case 'OPTIONS':
       if (typeof module.options !== 'undefined') handler = module.options();
-    break;
     case 'PATCH':
       if (typeof module.patch !== 'undefined') handler = module.patch();
-    break;
     case 'HEAD':
       if (typeof module.head !== 'undefined') handler = module.head();
-    break;
+    default:
+      if (typeof module.all !== 'undefined') handler = module.all();
   }
 
   return await handler(ctx, next);
