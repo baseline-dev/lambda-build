0b6a1bfbcd9484697c3e30823cc9dd49048ae569
Nikolai Onken <nikolai@nikolaionken.com>
2020-07-28 13:56:44 +0200

    Execute builds in parallel is CPU allows.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import glob from 'glob';
-import fs from 'fs';
-import path from 'path';
-import { spawnSync } from 'child_process';
+import { promises as fs } from 'fs';
+import path, { resolve } from 'path';
+import spawn from 'await-spawn';
+
 const rollupPkgPath = require.resolve('rollup/package.json'); 
 const rollupDir = path.dirname(rollupPkgPath);
 const rollupPkg = require(rollupPkgPath);
@@ -17,58 +18,67 @@ function buildLamda(routeDir, distDir, middlewarePath, fileName) {
     globPattern = path.join(routeDir, fileName);
   }
 
-  const routes = [];
-
   glob(globPattern, async function (er, files) {
-    files.forEach((file) => {
-      const parts = path.parse(path.relative(routeDir, file));
-      
-      const runtime = fs.readFileSync(path.join(__dirname, 'lib', 'runtime.js'), 'utf-8');      
-      
-      const output = runtime
-        .replace('{{import}}', `./${parts.base}`)
-        .replace('{{middlewaresPath}}', middlewarePath);
+    const operations = files.map((file) => {
+      return new Promise(async (resolve, reject) => {
+        try {
+          console.log(`Building: ${file}`);
 
-      let route = parts.dir;
-      let basename = parts.base;
-      if (basename !== 'index.js') {
-        route = path.join(route, parts.name);
-        basename = 'index.js';
-      }
-      
-      const fileName = `${parts.name}.build.js`;
-      let filePath = sanitize(route);
-      if (!filePath) filePath = 'root';
-
-      fs.writeFileSync(path.join(routeDir, parts.dir, fileName), output);
-      
-      const args = [
-        path.join(routeDir, parts.dir, fileName),
-        '--file', path.join(distDir, filePath, 'index.js'),
-        '--config', path.join(__dirname, 'rollup.config.js')
-      ];
-      
-      const parcel = spawnSync(binPath, args);
-      
-      if (parcel.status === 1) {
-        console.log(`Error generating: ${path.join(route, 'index.js')}`);
-        console.log(parcel.stderr.toString())
-      } else {
-        console.log(`Written output to: ${path.join(distDir, route, 'index.js')}`)
-      }
-
-      fs.unlinkSync(path.join(routeDir, parts.dir, fileName));
-
-      routes.push({
-        id: route ? route.replace(/[^a-zA-Z0-9]+/g, "-") : 'root',
-        path: `/${route}`,
-        buildDir: path.join(distDir, filePath),
-        fileSize: `${(fs.statSync(path.join(distDir, filePath, 'index.js')).size / 1000000).toFixed(2)} MB`
-      });
-
-    });
+          const parts = path.parse(path.relative(routeDir, file));
+        
+          const runtime = await fs.readFile(path.join(__dirname, 'lib', 'runtime.js'), 'utf-8');      
+          
+          const output = runtime
+            .replace('{{import}}', `./${parts.base}`)
+            .replace('{{middlewaresPath}}', middlewarePath);
     
-    fs.writeFileSync(path.join(distDir, 'routes.json'), JSON.stringify(routes, null, 2), 'utf8');
+          let route = parts.dir;
+          let basename = parts.base;
+          if (basename !== 'index.js') {
+            route = path.join(route, parts.name);
+            basename = 'index.js';
+          }
+          
+          const fileName = `${parts.name}.build.js`;
+          let filePath = sanitize(route);
+          if (!filePath) filePath = 'root';
+    
+          await fs.writeFile(path.join(routeDir, parts.dir, fileName), output);
+          
+          const args = [
+            path.join(routeDir, parts.dir, fileName),
+            '--file', path.join(distDir, filePath, 'index.js'),
+            '--config', path.join(__dirname, 'rollup.config.js')
+          ];
+          
+          const parcel = await spawn(binPath, args);
+          
+          if (parcel.status === 1) {
+            console.log(`Error generating: ${path.join(route, 'index.js')}`);
+            console.log(parcel.stderr.toString())
+          } else {
+            console.log(`Written output to: ${path.join(distDir, route, 'index.js')}`)
+          }
+    
+          await fs.unlink(path.join(routeDir, parts.dir, fileName));
+    
+          const stat = await fs.stat(path.join(distDir, filePath, 'index.js'));
+
+          resolve({
+            id: route ? route.replace(/[^a-zA-Z0-9]+/g, "-") : 'root',
+            path: `/${route}`,
+            buildDir: path.join(distDir, filePath),
+            fileSize: `${(stat.size / 1000000).toFixed(2)} MB`
+          });
+        } catch(e) {
+          reject(e);
+        }
+      });
+    });
+
+    const routes = await Promise.all(operations);
+    
+    await fs.writeFile(path.join(distDir, 'routes.json'), JSON.stringify(routes, null, 2), 'utf8');
 
     console.table(routes)
   });
