c18c2e2d9bc1249dc2772e85f26204a735391db0
Nikolai Onken <nikolai@nikolaionken.com>
2020-09-06 20:36:06 +0200

    Use koa router.

--- src/lib/runtime.js
+++ src/lib/runtime.js
@@ -1,48 +1,24 @@
 import serverless from 'serverless-http';
 import Koa from 'koa';
+import Router from '@koa/router';
 import * as module from '{{import}}';
 import {getMiddlewares} from '{{middlewaresPath}}';
 
 const app = new Koa();
+const router = new Router();
 
 app.context.gitSha = '{{gitSha}}';
 app.context.clientJs = '{{clientJs}}';
 
 getMiddlewares(app).forEach(middleware => app.use(middleware));
 
-app.use(async (ctx, next) => {
-  if (!ctx.time) {
-    ctx.time = +new Date();
-    console.log('New measurement handler: ', ctx.time / 1000);
-  }
+for (let method in module) {
+  let handler = routes[method]();
+  if (!Array.isArray(handler)) handler = [handler];
+  if (method === 'destroy') method = 'del';
+  router[method].apply(router, ['*'].concat(handler));
+}
 
-  let handler = (ctx) => { ctx.status = 404; }
-
-  switch (ctx.request.method) {
-    case 'GET':
-      if (typeof module.get !== 'undefined') handler = module.get();
-    case 'POST':
-      if (typeof module.post !== 'undefined') handler = module.post();
-    case 'PUT':
-      if (typeof module.put !== 'undefined') handler = module.put();
-    case 'DELETE':
-      if (typeof module.destroy !== 'undefined') handler = module.destroy();
-    case 'OPTIONS':
-      if (typeof module.options !== 'undefined') handler = module.options();
-    case 'PATCH':
-      if (typeof module.patch !== 'undefined') handler = module.patch();
-    case 'HEAD':
-      if (typeof module.head !== 'undefined') handler = module.head();
-    default:
-      if (typeof module.all !== 'undefined') handler = module.all();
-  }
-
-  const result = await handler(ctx, next);
-
-  const time = +new Date();
-  console.log('Timestamp end: ', (time - ctx.time) / 1000);
-
-  return result;
-});
+app.use(router.routes());
 
 exports.handler = serverless(app);
